67b7f4c44f76832a29be2fc3c161b786b8d3960f
Hardik <hardik12244@gmail.com>
2026-02-15 02:25:33 +0530

    feat:add footitem counter

--- frontend/src/components/FoodItem/FoodItem.tsx
+++ frontend/src/components/FoodItem/FoodItem.tsx
@@ -1,23 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { assets, type FoodItemType } from '../../assets/assets'
 import './FoodItem.css'
 
-function FoodItem({_id,name,description,price,image}:FoodItemType) {
-  return (
-    <div className='food-item'>
-        <div className="food-item-img-container">
-            <img className='food-item-image' src={image} alt="" />
-        </div>
-        <div className="food-item-info">
-            <div className="food-item-name-rating">
-                <p>{name}</p>
-                <img src={assets.rating_starts} alt="" />
+function FoodItem({ _id, name, description, price, image }: FoodItemType) {
+
+    const [itemCount, setItemCount] = useState(0);
+
+    return (
+        <div className='food-item'>
+            <div className="food-item-img-container">
+                <img className='food-item-image' src={image} alt="" />
+                {!itemCount ? <img onClick={() => setItemCount(prev => prev + 1)} className='add' src={assets.add_icon_white} /> : <div className='food-item-counter'>
+                    <img onClick={() => setItemCount(prev => prev - 1)} src={assets.remove_icon_red} alt="" />
+                    <p>{itemCount}</p>
+                    <img onClick={() => setItemCount(prev => prev + 1)} src={assets.add_icon_green} alt="" />
+                </div>}
             </div>
-            <p className="food-item-desc">{description}</p>
-            <p className='food-item-price'>${price}</p>
+            <div className="food-item-info">
+                <div className="food-item-name-rating">
+                    <p>{name}</p>
+                    <img src={assets.rating_starts} alt="" />
+                </div>
+                <p className="food-item-desc">{description}</p>
+                <p className='food-item-price'>${price}</p>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default FoodItem
