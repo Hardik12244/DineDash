8fcf703346ebecd8226972167a014f5c53920c81
Hardik <hardik12244@gmail.com>
2026-02-15 03:01:00 +0530

    feat:add cartItems counter context

--- frontend/src/components/FoodItem/FoodItem.tsx
+++ frontend/src/components/FoodItem/FoodItem.tsx
@@ -1,19 +1,22 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { assets, type FoodItemType } from '../../assets/assets'
 import './FoodItem.css'
+import { StoreContext } from '../../context/StoreContext'
 
 function FoodItem({ _id, name, description, price, image }: FoodItemType) {
 
-    const [itemCount, setItemCount] = useState(0);
+    const context = useContext(StoreContext);
+    if (!context) return null;
 
+    const { cartItems, addToCart, removeFromCart } = context;
     return (
         <div className='food-item'>
             <div className="food-item-img-container">
                 <img className='food-item-image' src={image} alt="" />
-                {!itemCount ? <img onClick={() => setItemCount(prev => prev + 1)} className='add' src={assets.add_icon_white} /> : <div className='food-item-counter'>
-                    <img onClick={() => setItemCount(prev => prev - 1)} src={assets.remove_icon_red} alt="" />
-                    <p>{itemCount}</p>
-                    <img onClick={() => setItemCount(prev => prev + 1)} src={assets.add_icon_green} alt="" />
+                {!cartItems[_id] ? <img onClick={() => addToCart(_id)} className='add' src={assets.add_icon_white} /> : <div className='food-item-counter'>
+                    <img onClick={() => removeFromCart(_id)} src={assets.remove_icon_red} alt="" />
+                    <p>{cartItems[_id]}</p>
+                    <img onClick={() => addToCart(_id)} src={assets.add_icon_green} alt="" />
                 </div>}
             </div>
             <div className="food-item-info">
--- frontend/src/context/StoreContext.tsx
+++ frontend/src/context/StoreContext.tsx
@@ -1,21 +1,54 @@
-import { createContext, type ReactNode } from "react";
+import { createContext, useEffect, useState, type ReactNode } from "react";
 import { food_list, type FoodItemType } from "../assets/assets";
 
 type StoreContextType = {
     food_list: FoodItemType[];
+    cartItems:CartItemsType;
+    setCartItems:React.Dispatch<React.SetStateAction<CartItemsType>>;
+    addToCart: (itemId: string) => void;
+    removeFromCart: (itemId: string) => void;
 }
-
-export const StoreContext = createContext<StoreContextType | null>(null);
 
 type StoreContextProviderProps = {
     children: ReactNode;
 }
 
+type CartItemsType={
+    [key:string]:number;
+}
+
+export const StoreContext = createContext<StoreContextType | null>(null);
+
+
+
 const StoreContextProvider = ({ children }: StoreContextProviderProps) => {
 
-    const contextValue: StoreContextType = {
-        food_list
+
+
+    const [cartItems, setCartItems] = useState<CartItemsType>({});
+
+    const addToCart = (itemId: string) => {
+        if (!cartItems[itemId]) {
+            setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
+        } else {
+            setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
+        }
     }
+    const removeFromCart = (itemId: string) => {
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+    }
+    useEffect(() => {
+
+    }, [cartItems])
+
+    const contextValue: StoreContextType = {
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart,
+    }
+
     return (
         <StoreContext.Provider value={contextValue}>
             {children}
