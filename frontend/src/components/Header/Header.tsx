9d9a1da3d1f80d2b69d0b8617231058c2e6be09a
Hardik <hardik12244@gmail.com>
2026-02-14 20:27:12 +0530

    feat:add header section

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <div className='header'>
+            <div className='header-content'>
+                <h2>Order your favourite food</h2>
+                <p>Choose form a diverse menu</p>
+                <button>View menu</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
