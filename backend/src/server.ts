e5d932a82e652ac1ea88c73573d692a37c97010e
Hardik <hardik12244@gmail.com>
2026-02-17 23:43:54 +0530

    backend setup

---
+++ backend/src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Backend running");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
